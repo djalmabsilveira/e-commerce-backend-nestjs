c7feff9eb2dc644ac4502c87d318790750fa1de2
Djalma Silveira <djalmasilveira10@gmail.com>
2023-10-09 22:42:08 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { CustomersModule } from './customers/customers.module';
+import { CartModule } from './cart/cart.module';
+import { AuthModule } from './auth/auth.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { PaymentModule } from './payment/payment.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { ReportsModule } from './reports/reports.module';
+import { AdminModule } from './admin/admin.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    CategoriesModule,
+    OrdersModule,
+    CustomersModule,
+    CartModule,
+    AuthModule,
+    ReviewsModule,
+    PaymentModule,
+    NotificationsModule,
+    ReportsModule,
+    AdminModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
