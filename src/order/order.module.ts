08e6a9f503ce470074b5b1cab9b4f71c9c8a05bf
Djalma Silveira <djalmasilveira10@gmail.com>
2023-10-14 19:42:43 -0300

    bugfix(OrderModule):  fix exports

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
-import { Order } from './entities/order.entity';
+import { OrderService } from './order.service';
 
 @Module({
   controllers: [OrderController],
   providers: [OrderService],
-  exports:[Order,]
 })
 export class OrderModule {}
